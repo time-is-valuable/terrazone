c9284d38b65b349e16ee565d7a8e7f75c53792fa
Jesse Winton <jesse@appwrite.io>
2024-07-09 09:45:41 -0400

    initial commit

---
+++ apps/web/src/components/globe.tsx
@@ -0,0 +1,105 @@
+'use client';
+
+import createGlobe, { type Marker } from 'cobe';
+import { useEffect, useRef } from 'react';
+import { useSpring } from 'react-spring';
+
+export const Globe = () => {
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+  const pointerInteracting = useRef<null | number>(null);
+  const pointerInteractionMovement = useRef(0);
+
+  const [{ r }, api] = useSpring(() => ({
+    r: 0,
+    config: {
+      mass: 1,
+      tension: 280,
+      friction: 40,
+      precision: 0.001,
+    },
+  }));
+
+  const markers: Marker[] = [
+    { location: [37.7595, -122.4367], size: 0.03 },
+    { location: [40.7128, -74.006], size: 0.1 },
+  ];
+
+  useEffect(() => {
+    let width = 0;
+    const onResize = () =>
+      canvasRef.current && (width = canvasRef.current.offsetWidth);
+    window.addEventListener('resize', onResize);
+    onResize();
+
+    if (!canvasRef.current) {
+      return;
+    }
+
+    const globe = createGlobe(canvasRef.current, {
+      devicePixelRatio: 2,
+      width: 600 * 2,
+      height: 600 * 2,
+      phi: 0,
+      theta: 0,
+      dark: 1,
+      diffuse: 1.2,
+      mapSamples: 16000,
+      mapBrightness: 6,
+      baseColor: [0.3, 0.3, 0.3],
+      markerColor: [0.1, 0.8, 1],
+      glowColor: [1, 1, 1],
+      markers,
+      onRender: (state) => {
+        state.phi = r.get();
+        state.width = width * 2;
+        state.height = width * 2;
+      },
+    });
+
+    setTimeout(() => (canvasRef.current!.style.opacity = '1'));
+    return () => {
+      globe.destroy();
+      window.removeEventListener('resize', onResize);
+    };
+  }, []);
+
+  return (
+    <>
+      <canvas
+        ref={canvasRef}
+        className="w-[600px] h-[600px] max-w-full aspect-[1]"
+        onPointerDown={(e) => {
+          pointerInteracting.current =
+            e.clientX - pointerInteractionMovement.current;
+          canvasRef.current!.style.cursor = 'grabbing';
+        }}
+        onPointerUp={() => {
+          pointerInteracting.current = null;
+          canvasRef.current!.style.cursor = 'grab';
+        }}
+        onPointerOut={() => {
+          pointerInteracting.current = null;
+          canvasRef.current!.style.cursor = 'grab';
+        }}
+        onMouseMove={(e) => {
+          if (pointerInteracting.current !== null) {
+            const delta = e.clientX - pointerInteracting.current;
+            pointerInteractionMovement.current = delta;
+            api.start({
+              r: delta / 200,
+            });
+          }
+        }}
+        onTouchMove={(e) => {
+          if (pointerInteracting.current !== null && e.touches[0]) {
+            const delta = e.touches[0].clientX - pointerInteracting.current;
+            pointerInteractionMovement.current = delta;
+            api.start({
+              r: delta / 100,
+            });
+          }
+        }}
+      />
+    </>
+  );
+};
