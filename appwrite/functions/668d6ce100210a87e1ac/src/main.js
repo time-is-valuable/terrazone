479fdcac0dd337fb406e8f4ba0e0aa22f3adc794
Jesse Winton <jesse@appwrite.io>
2024-07-11 10:54:20 -0400

    add functions

---
+++ appwrite/functions/668d6ce100210a87e1ac/src/main.js
@@ -0,0 +1,55 @@
+import {ID, InputFile, Permission, Role} from 'node-appwrite';
+import fetch from "node-fetch";
+import AppwriteClient from "./shared/appwriteClient.js";
+
+export default async ({ req, res, log, error }) => {
+    if (req.headers['x-appwrite-trigger'] !== 'event') {
+        return res.json({});
+    }
+    AppwriteClient.setClient();
+    const user = req.bodyJson;
+
+    let photoId = '';
+    const defaultTimezone = process.env.DEFAULT_TIMEZONE ?? 'UTC';
+    const addingToTeam = process.env.ADDING_TO_TEAM === 'enabled' ?? false;
+
+    try {
+        const buffer = await fetch(`https://unavatar.io/${user.email}`)
+        const response = await AppwriteClient.getStorage().createFile(
+            AppwriteClient.bucketsId,
+            ID.unique(),
+            InputFile.fromBuffer(Buffer.from((await buffer.arrayBuffer())), `${user.$id}.jpg`),
+            [
+                Permission.update(Role.user(user.$id)),
+            ]
+        );
+
+        photoId = response.$id;
+    } catch (e) {
+        // We can continue without an image.
+        error(e);
+    }
+
+    try {
+        await AppwriteClient.getDatabases().createDocument(AppwriteClient.databaseId, AppwriteClient.collectionId, ID.unique(), {
+            times: '{}',
+            id: user.$id,
+            name: user.name,
+            timezone: defaultTimezone,
+            location: '',
+            photo_id: photoId
+        }, [
+            Permission.read(Role.any()),
+            Permission.update(Role.user(user.$id)),
+        ]);
+
+        if (addingToTeam) {
+            await AppwriteClient.getTeams().createMembership(AppwriteClient.teamsId, [], user.email);
+        }
+    } catch (e) {
+        error(e);
+        return res.json({ success: false }, 500);
+    }
+
+    return res.json({ success: true }, 200);
+};
